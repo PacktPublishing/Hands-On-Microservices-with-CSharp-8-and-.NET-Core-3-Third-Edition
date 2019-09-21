62d2c5d1b8cdb3eace5f26383555d38127993e47
Gaurav Aroraa <g_arora@hotmail.com>
2019-09-22 03:30:09 +0530

    Changed swagger usage

--- Chapter 02/FlixOne.BookStore.ProductService/Startup.cs
+++ Chapter 02/FlixOne.BookStore.ProductService/Startup.cs
@@ -76,13 +76,12 @@ namespace FlixOne.BookStore.ProductService
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
+
             app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
+            app.UseSwaggerUI(option =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API V1");
+                option.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API V1");
             });
         }
     }

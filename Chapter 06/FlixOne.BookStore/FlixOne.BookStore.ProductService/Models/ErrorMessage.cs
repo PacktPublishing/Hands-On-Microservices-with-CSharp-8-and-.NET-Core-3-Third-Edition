bf45654c6ffc5cf86b94b1f24cd500086c529447
Gaurav Aroraa <g_arora@hotmail.com>
2019-11-06 07:17:25 +0530

    Implement Azure Active Directory and OpenConnect ID to Product Services

---
+++ Chapter 06/FlixOne.BookStore/FlixOne.BookStore.ProductService/Models/ErrorMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlixOne.BookStore.ProductService.Models
+{
+    public class ErrorMessage
+    {
+        public Guid Id { get; set; }
+        public int StatusCode { get; set; }
+        public string Error { get; set; }
+    }
+}
---
+++ Chapter 06/FlixOne.BookStore/FlixOne.BookStore.ProductService/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace FlixOne.BookStore.ProductService
+{
+    public class Startup
+    {
+        public static string Impersonation;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(op =>
+            {
+                op.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Product API", Version = "v1" });
+                op.DescribeAllParametersInCamelCase();
+                //op.DescribeAllEnumsAsStrings();
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                op.IncludeXmlComments(xmlPath);
+                op.CustomSchemaIds(x => x.FullName);
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            Impersonation = Configuration["AzureAd:impersonation"];
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+             
+            app.UseSwagger();
+
+            app.UseSwaggerUI(op =>
+            {
+                op.SwaggerEndpoint("/swagger/v1/swagger.json", "Product APIs");
+                op.RoutePrefix = string.Empty;
+            });
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

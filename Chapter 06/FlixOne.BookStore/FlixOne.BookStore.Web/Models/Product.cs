7732fade01e5e957eaf3492622dffc1df0be54aa
Gaurav Aroraa <g_arora@hotmail.com>
2019-11-09 05:00:11 +0530

    minor fixes for Chap6

---
+++ Chapter 06/FlixOne.BookStore/FlixOne.BookStore.Web/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlixOne.BookStore.Web.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public string CreatedBy { get; set; }
+    }
+}

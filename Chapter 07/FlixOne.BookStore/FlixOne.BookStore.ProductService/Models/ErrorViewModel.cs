1fbb5d86c333d7af4a70b99a172154b75becb037
Gaurav Aroraa <g_arora@hotmail.com>
2019-11-18 21:27:15 +0530

    Completed Chapter 7

--- Chapter 07/FlixOne.BookStore/FlixOne.BookStore.ProductService/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FlixOne.BookStore.ProductService.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

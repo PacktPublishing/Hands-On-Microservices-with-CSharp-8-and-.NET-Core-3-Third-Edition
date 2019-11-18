7eae1496950b2e8ce38590aecc13666e84961731
Gaurav Aroraa <g_arora@hotmail.com>
2019-11-18 21:15:49 +0530

    Chapter 7- Applciation Insights example

---
+++ Chapter 07/FlixOne.BookStore/FlixOne.BookStore.ProductService/Migrations/20170919201253_ProductDB.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FlixOne.BookStore.ProductService.Migrations
+{
+    public partial class ProductDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18, 2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ Chapter 07/FlixOne.BookStore/FlixOne.BookStore.ProductService/Persistence/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlixOne.BookStore.ProductService.Contexts;
+using FlixOne.BookStore.ProductService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlixOne.BookStore.ProductService.Persistence
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _context;
+
+        public ProductRepository(ProductContext context) => _context = context;
+
+        public void Add(Product product)
+        {
+            _context.Add(product);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Product> GetAll() => _context.Products.Include(c => c.Category).ToList();
+
+        public async Task<IEnumerable<Product>> GetAllAsync() => await _context.Products.Include(c => c.Category).ToListAsync();
+
+        public Product GetBy(Guid id) => _context.Products.Include(c => c.Category).FirstOrDefault(x => x.Id == id);
+
+        public void Remove(Guid id)
+        {
+            var product = GetBy(id);
+            _context.Remove(product);
+        }
+
+        public void Update(Product product) => _context.Update(product);
+    }
+}

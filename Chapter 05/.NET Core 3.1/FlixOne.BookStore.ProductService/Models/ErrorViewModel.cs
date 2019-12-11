987bfa6bf82a2a93f321197e92bfe14f400edbc0
Gaurav Aroraa <g_arora@hotmail.com>
2019-12-12 03:46:34 +0530

    Chapter 5 - updated code to .NET Core 3.1

---
+++ Chapter 05/.NET Core 3.1/FlixOne.BookStore.ProductService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FlixOne.BookStore.ProductService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

526bc13f3562662f34c021a22136e4e17fb2ad54
Gaurav Aroraa <g_arora@hotmail.com>
2020-02-13 19:41:14 +0530

    Chapter 10 - added more images

--- Chapter 10/FlixOne.BookStore.OfferService/FlixOne.BookStore.OfferService/Models/Vendor.cs
+++ Chapter 10/FlixOne.BookStore.OfferService/FlixOne.BookStore.OfferService/Models/Vendor.cs
@@ -7,7 +7,6 @@ namespace FlixOne.BookStore.OfferService.Models
     {
         public Guid Id { get; set; }
         public string Code { get; set; }
-
         public string Name { get; set; }
         public string Description { get; set; }
         public string URL { get; set; }

a2b4d69f00a55f254cc7bdcf0c3c8a1bee357d6c
Gaurav Aroraa <g_arora@hotmail.com>
2020-02-27 21:39:28 +0530

    Basic API Gateway - OCelot

---
+++ Chapter 10/Api Gateway pattern/Basic/FlixOne.BookStore.VendorService/Migrations/20200227153526_FlixOneVendorDB.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlixOne.BookStore.VendorService.Migrations
+{
+    public partial class FlixOneVendorDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vendors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Code = table.Column<string>(type: "varchar(15)", nullable: true),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Description = table.Column<string>(type: "varchar(400)", nullable: true),
+                    URL = table.Column<string>(type: "varchar(250)", nullable: true),
+                    Logo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddedOn = table.Column<DateTime>(nullable: false),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    AddressLine1 = table.Column<string>(type: "varchar(150)", nullable: true),
+                    AddressLine2 = table.Column<string>(type: "varchar(150)", nullable: true),
+                    City = table.Column<string>(type: "varchar(50)", nullable: true),
+                    State = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Country = table.Column<string>(type: "varchar(50)", nullable: true),
+                    PIN = table.Column<string>(type: "varchar(8)", nullable: true),
+                    VendorId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_Vendors_VendorId",
+                        column: x => x.VendorId,
+                        principalTable: "Vendors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_VendorId",
+                table: "Addresses",
+                column: "VendorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Vendors");
+        }
+    }
+}
---
+++ Chapter 10/Api Gateway pattern/Basic/FlixOne.BookStore.VendorService/Models/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlixOne.BookStore.VendorService.Models
+{
+    public class Address
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Column(TypeName = "varchar(150)")]
+        public string AddressLine1 { get; set; }
+
+        [Column(TypeName = "varchar(150)")]
+        public string AddressLine2 { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string City { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string State { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Country { get; set; }
+
+        [Column(TypeName = "varchar(8)")]
+        public string PIN { get; set; }
+
+        public Guid VendorId { get; set; }
+
+        public virtual Vendor Vendor { get; set; }
+    }
+}

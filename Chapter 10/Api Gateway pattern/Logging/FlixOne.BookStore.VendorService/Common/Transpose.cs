205b48e7ce5e005732aec0091201893653b347e7
Gaurav Aroraa <g_arora@hotmail.com>
2020-03-01 01:28:23 +0530

    Logging API Gateway - Ocelot

---
+++ Chapter 10/Api Gateway pattern/Logging/FlixOne.BookStore.VendorService/Common/Transpose.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlixOne.BookStore.VendorService.Common
+{
+    public static class Transpose
+    {
+        public static Models.Vendor ToModel(this Models.VendorViewModel vm)
+        {
+            var vendor = new Models.Vendor
+            {
+                Id = new System.Guid(vm.Id),
+                Active = vm.Active,
+                AddedOn = vm.AddedOn,
+                Code = vm.Code,
+                Description = vm.Description,
+                Logo = vm.Logo,
+                Name = vm.Name,
+                URL = vm.URL
+            };
+            if (vm.Address != null)
+            {
+                var address = vm.Address;
+                vendor.Addresses = new List<Models.Address>
+                {
+                new Models.Address
+                {
+                    Id = address.Id,
+                    AddressLine1 = address.AddressLine1,
+                    AddressLine2 = address.AddressLine2,
+                    City = address.City,
+                    Country = address.Country,
+                    State = address.State,
+                    PIN = address.PIN,
+                    VendorId = address.VendorId
+                }
+                };
+            }
+            return vendor;
+        }
+
+        public static IEnumerable<Models.Vendor> ToModel(this IEnumerable<Models.VendorViewModel> vm) => vm.Select(item => item.ToModel()).ToList();
+
+        public static Models.VendorViewModel ToViewModel(this Models.Vendor model)
+        {
+            var vendorVM = new Models.VendorViewModel
+            {
+                Id = model.Id.ToString(),
+                Active = model.Active,
+                AddedOn = model.AddedOn,
+                Code = model.Code,
+                Description = model.Description,
+                Logo = model.Logo,
+                Name = model.Name,
+                URL = model.URL
+            };
+
+            if (model.Addresses.Any())
+            {
+                var address = model.Addresses.FirstOrDefault();
+
+                vendorVM.Address = new Models.AddressViewModel
+                {
+                    Id = address.Id,
+                    AddressLine1 = address.AddressLine1,
+                    AddressLine2 = address.AddressLine2,
+                    City = address.City,
+                    Country = address.Country,
+                    State = address.State,
+                    PIN = address.PIN,
+                    VendorId = address.VendorId
+                };
+            }
+
+            return vendorVM;
+        }
+
+        public static IEnumerable<Models.VendorViewModel> ToViewModel(this IEnumerable<Models.Vendor> model) => model.Select(item => item.ToViewModel()).ToList();
+    }
+}
---
+++ Chapter 10/Api Gateway pattern/Logging/FlixOne.BookStore.VendorService/Persistence/IVendorRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using FlixOne.BookStore.VendorService.Models;
+
+namespace FlixOne.BookStore.VendorService.Persistence
+{
+    public interface IVendorRepository
+    {
+        void Add(Models.Vendor vendor);
+
+        List<Models.Vendor> GetAll();
+
+        Models.Vendor GetBy(Guid id);
+
+        void Remove(Guid id);
+
+        void Update(Models.Vendor vendor);
+    }
+}

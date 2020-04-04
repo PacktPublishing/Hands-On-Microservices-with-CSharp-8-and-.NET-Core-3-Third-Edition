98930765ea5f5e2c0d1e016a93a56c8aff124449
Gaurav Aroraa <g_arora@hotmail.com>
2020-04-05 03:05:49 +0530

    First pass Xamarin tempate

---
+++ Chapter 11/.NET Core 3.1/Mobile/xam/FlixOne.BookStore/FlixOne.BookStore/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FlixOne.BookStore.Services;
+using FlixOne.BookStore.Views;
+
+namespace FlixOne.BookStore
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}

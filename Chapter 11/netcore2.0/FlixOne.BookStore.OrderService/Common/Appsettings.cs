6c6b179ffcf78865a8bbfbb17628d789e6dc6548
Gaurav Aroraa <g_arora@hotmail.com>
2020-04-05 02:25:55 +0530

    First pass - angular template

--- Chapter 11/netcore2.0/FlixOne.BookStore.OrderService/Common/Appsettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FlixOne.BookStore.OrderService.Common
-{
-    //appsettings.json fetcher class
-    public class AppSettings
-    {
-        public string ProductServiceUri { get; set; }
-    }
-}
